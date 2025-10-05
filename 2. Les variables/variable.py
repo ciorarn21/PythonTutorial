8601dab72d43080d8a9f09a2820911c09cf6a32b
ciorarn21 <48468963+ciorarn21@users.noreply.github.com>
2025-10-05 14:35:39 +0200

    Add files via upload

---
+++ 2. Les variables/variable.py
@@ -0,0 +1,19 @@
+
+# La saisi des variables
+
+numero : int = 5
+numeroB : int = 10
+
+print(numero + numeroB)
+
+# La récupération des inputs
+
+saisi : str = input("Où sont les dias de Bastin ?\n")
+
+print("N'oubliez pas de presser enter pour confirmer votre saisi hein",end="\n")
+
+print("Les dias se trouvent ...", saisi)
+
+
+
+
