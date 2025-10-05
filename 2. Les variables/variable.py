58733507f4af29cdbe7ae8b7341aff59ff01532a
ciorarn21 <48468963+ciorarn21@users.noreply.github.com>
2025-10-05 14:48:42 +0200

    Update variable.py

--- 2. Les variables/variable.py
+++ 2. Les variables/variable.py
@@ -14,6 +14,19 @@ print("N'oubliez pas de presser enter pour confirmer votre saisi hein",end="\n")
 
 print("Les dias se trouvent ...", saisi)
 
+# Pour le formattage des strings ..
 
+variable : int = 4520
+print("La valeur est :", variable)
+
+# Vous pouvez très bien faire ceci :
+
+print(f"La valeur est {variable}") # Ici les accolades servent à indiquer la variable à afficher
+
+# Mais la manière suivante est tout à fait juste aussi :
+
+phrase : str = f"La valeur est {variable}"
+
+print(phrase) # Ca fonctionne exactement de la même manière.
 
 
